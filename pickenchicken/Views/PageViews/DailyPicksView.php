f512de7efdeb75fd764a6c36954e5f0926376de5
James P Williams <James@Macs-MacBook-Air-2.local>
2021-11-08 17:17:01 -0500

    improved daily picks view

--- pickenchicken/Views/PageViews/DailyPicksView.php
+++ pickenchicken/Views/PageViews/DailyPicksView.php
@@ -6,9 +6,22 @@ use \bandpress\Views\View;
 class DailyPicksView extends View{
 
     public function renderBody(){
-        $games = $this->data->getGames();
+        
         $allGamesFinished = $allGamesStarted = false;
         $userPicks = $this->data->getUserPicks(app()->currentUser()->id());
+
+        $this->renderIntro();
+
+        if(empty($userPicks)){
+            
+            $this->renderPicksForm();
+            
+        }
+        else{
+            $this->renderScoreboard();    
+            $this->renderGames();
+        }
+die;
         $chickenResults = $userResults = array("win"=>0,"loss"=>0,"push"=>0);
         foreach($games as $i=>$game){
             
@@ -38,8 +51,7 @@ class DailyPicksView extends View{
 
         ?>
         
-        <h2 class="d-flex justify-content-center">The Chicken's Pickens!</h2>
-        <h6 class="d-flex justify-content-center">Games for <?php echo $this->data->title();?></h6>
+        
         <div class="container mb-5">
             <div class="row">
                 <div class="col text-center">
@@ -73,19 +85,27 @@ class DailyPicksView extends View{
             </div>
             <?php endif;?>
         </div>
-        <div class="container">
+        
+        <?php
+    }
+
+    public function renderIntro(){
+        ?>
+        <h2 class="d-flex justify-content-center">The Chicken's Pickens!</h2>
+        <h6 class="d-flex justify-content-center">Games for <?php echo $this->data->title();?></h6>
+        <?php
+
+    }
+
+    public function renderPicksForm(){
+        $games = $this->data->getGames();
+
+?>
+<div class="container">
             <form action="actions/dailyPicks" method="post">
             <input type="hidden" name="postId" value="<?= $this->data->id();?>"/>
             <?php foreach ($games as $i=>$game): 
-                $disabled = "";//$game->gameStartedInThePast()?"disabled":"";
-                $userPick = array_shift($userPicks); 
-
-                $homeSelected = ($userPick=="home")?"checked":"";
-                $awaySelected = ($userPick=="away")?"checked":"";
-                $textClass="";
-
-                $team = $game->teamFromHomeAway($game->chickenPick());
-            ?>
+?>
             <div class="row justify-content-center">
                 <div class="col text-center" id="away">
                 <?= $game->awayTeam()->abbreviation();?>
@@ -100,39 +120,6 @@ class DailyPicksView extends View{
                 <input type="radio" name="gamePicks[<?=$i?>]" value="away" <?= $disabled; ?> <?= $awaySelected;?> />
                 </div>
                 <div class="col text-center">
-                <?php if ($game->gameIsDecided()):?>
-                <div><?php echo $game->awayScore();?> - <?php echo $game->homeScore();?></div>
-                <?php
-                    if($game->winningPick()=="push"){
-                        $text="Push";
-                        $textClass="text-warning";
-                        $userPicks["push"]++;
-                    }
-                    else{
-                        if($game->pickIsWinner($userPick)){
-                            $text="You Win!";
-                            $textClass="text-success";
-                        }
-                        else{
-                            $text="You lose.";
-                            $textClass="text-danger";
-                        }
-                    }
-                ?>
-                    <div class="userPickResult <?=$textClass;?>"><?=$text;?></div>
-                    <?php
-                        if($game->winningPick()=="push"){
-                            $textClass="text-warning";
-                        }
-                        else{
-                            $textClass=$game->pickIsWinner($game->chickenPick())?"text-success":"text-danger";
-                        }
-                        
-                        
-                    ?>
-                    
-                <?php endif;?>
-                <div class="chickenPickResult">Chicken Picked <span class='<?=$textClass;?>'><?=$team->abbreviation()?></span></div>
                 </div>
                 <div class="col text-center" id="home">
                 <input type="radio" name="gamePicks[<?=$i?>]" value="home" <?= $disabled;?> <?= $homeSelected;?>/>
@@ -148,6 +135,158 @@ class DailyPicksView extends View{
             </div>
             </form>
         </div>
+<?php
+    }
+
+    private function renderGames(){
+       $userPicks = $this->data->getUserPicks(app()->currentUser()->id());
+        ?>
+        <div class="container">
+
+            <?php foreach ($this->data->getGames() as $i=>$game): 
+                $disabled = "";//$game->gameStartedInThePast()?"disabled":"";
+                $userPick = $userPicks[$i];
+                $textClass="";
+                $userTeam = $game->teamFromHomeAway($userPick);
+                $chickenTeam = $game->teamFromHomeAway($game->chickenPick());
+            ?>
+            <div class="row justify-content-center">
+                <div class="col text-center">
+                    <strong>Away</strong>
+                </div>
+                <div class="col text-center">
+                    
+                </div>
+                <div class="col text-center">
+                    <strong>Home</strong>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col text-center" id="away">
+                <?= $game->awayTeam()->abbreviation();?>
+                </div>
+                <div class="col text-center"><?= $game->displayPointSpread();?></div>
+                <div class="col text-center" id="home">
+                <?= $game->homeTeam()->abbreviation();?>
+                </div>
+            </div>
+            <div class="row mb-3 justify-content-center">
+                <div class="col text-center" id="away">
+                
+                </div>
+                <div class="col text-center">
+                <?php if ($game->gameIsDecided()):?>
+                <div><?php echo $game->awayScore();?> - <?php echo $game->homeScore();?></div>
+                <?php
+                    if($game->winningPick()=="push"){
+                        $text="Push";
+                        $textClass="text-warning";
+                        $userPicks["push"]++;
+                    }
+                    else{
+                        if($game->pickIsWinner($userPick)){
+                            $text="You picked {$userTeam->abbreviation()}. You Win!";
+                            $textClass="text-success";
+                        }
+                        else{
+                            $text="You picked {$userTeam->abbreviation()}. You lose.";
+                            $textClass="text-danger";
+                        }
+                    }
+                ?>
+                    <div class="userPickResult <?=$textClass;?>"><?=$text;?></div>
+                    <?php
+                        if($game->winningPick()=="push"){
+                            $textClass="text-warning";
+                        }
+                        else{
+                            $textClass=$game->pickIsWinner($game->chickenPick())?"text-success":"text-danger";
+                        }
+                        
+                        
+                    ?>
+                <?php else:?>
+                    <div class="userPickResult">You Picked <span class='<?=$textClass;?>'><?=$userTeam->abbreviation()?></span></div>
+                <?php endif;?>
+                <div class="chickenPickResult">Chicken Picked <span class='<?=$textClass;?>'><?=$chickenTeam->abbreviation()?></span></div>
+                
+                </div>
+                <div class="col text-center" id="home">
+                
+                </div>
+            </div>
+
+            <?php endforeach;?>
+            <?php $disabled = $allGamesStarted?"disabled":"";?>
+            
+        </div>
+        <?php
+    }
+
+    public function renderScoreboard(){
+        
+        $chickenResults = $userResults = array("win"=>0,"loss"=>0,"push"=>0);
+        foreach($this->data->getGames() as $i=>$game){
+            
+            //$allGamesStarted = $allGamesStarted && $game->gameStartedInThePast();
+            $allGamesFinished = $allGamesStarted && $game->gameIsDecided();
+            if($game->gameIsDecided()){
+                if($game->winningPick()=="push"){
+                    $chickenResults['push']++;
+                    $userResults['push']++;
+                }
+                else{
+                    if($game->pickIsWinner($game->chickenPick())){
+                        $chickenResults['win']++;
+                    }
+                    else{
+                        $chickenResults['loss']++;
+                    }
+                    if($game->pickIsWinner($userPicks[$i])){
+                        $userResults['win']++;
+                    }
+                    else{
+                        $userResults['loss']++;
+                    }
+                }
+            }
+        }
+
+        ?>
+        
+        
+        <div class="container mb-5">
+            <div class="row">
+                <div class="col text-center">
+                    <h6>Your results: <?= $userResults['win'] . "-".$userResults['loss']."-".$userResults['push']?></h6>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <h6>Chicken's results: <?= $chickenResults['win'] . "-".$chickenResults['loss']."-".$chickenResults['push']?></h6>
+                </div>
+            </div>
+            <?php if ($allGamesFinished):
+            if($userResults['win']>$chickenResults['win']){
+                $text = "You plucked the chicken!";
+                $textClass="text-success";
+            }
+            elseif($userResults['win']<$chickenResults['win']){
+                $text = "You got pecked by the chicken!";
+                $textClass="text-danger";
+            }
+            else{
+                $text="You equaled a chicken.";
+                $textClass="text-danger";
+            }
+                
+            ?>
+            <div class="row">
+            <div class="col text-center">
+                <h6 class="<?=$textClass;?>"><?=$text?></h6>
+            </div>
+            </div>
+            <?php endif;?>
         <?php
     }
 }
