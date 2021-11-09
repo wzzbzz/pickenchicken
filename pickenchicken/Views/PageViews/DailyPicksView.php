b9b3685138d540fff155f787f546a9c2d0e36829
James P Williams <James@Macs-MacBook-Air-2.local>
2021-11-09 07:14:30 -0500

    testing

--- pickenchicken/Views/PageViews/DailyPicksView.php
+++ pickenchicken/Views/PageViews/DailyPicksView.php
@@ -257,6 +257,7 @@ die;
                         $allResults['TheChicken']['loss']++;
                     }
                     foreach($usersPicks as $user_id=>$userPick){
+                        diebug($userPick);
                         if($game->pickIsWinner($userPick[$i])){
                             $allResults[$user_id]['win']++;
                         }
